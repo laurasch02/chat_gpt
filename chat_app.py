8cb1ede1cb3f74c01f1ceeee0cd489d1e3081668
laurasch02 <123311778+laurasch02@users.noreply.github.com>
2023-01-28 16:08:05 +0000

    modified the lines to fit the pylint instructions

--- chat_app.py
+++ chat_app.py
@@ -6,7 +6,7 @@ model = openai.Model.retrieve("text-davinci-003")
 
 st.title("Chat with something more intelligent than you!")
 st.sidebar.header("Instructions")
-st.sidebar.info("""Just aks any question you want and the chat bot will give you an answer! 
+st.sidebar.info("""Just aks any question you want and the chat bot will give you an answer!
 Depending on your task, you can choose different models:\n
 model 1: 'text-davinci-003' very good for question and answer\n
 model 2: 'curie' for sentiment and summarization\n
@@ -15,11 +15,13 @@ model_input = st.text_input("Choose the model you want to use!")
 prompt_input = st.text_input("Ask a question!", "Why are vegan people superior?")
 
 def ChatGPT():
+    """ asks the chat engine for a completion to the prompt set by the user"""
     completion = openai.Completion.create(engine=model_input, prompt=prompt_input, max_tokens = 2000, temperature=0.7)
     message = completion.choices[0].text
     return message
 
 def main():
+    """runs the Chat GPT method and return the completion"""
     response = ChatGPT()
     return st.write(f"{response}")
 
